3fe1834af6a564792e0ecc1bb23e20409cb29ce3
MatinaDataEngineer <33282894+MatinaDataEngineer@users.noreply.github.com>
2020-02-15 15:20:06 +0300

    Update rds_config.py

--- pythonscripts/rds_config.py
+++ pythonscripts/rds_config.py
@@ -1,4 +1,5 @@
 #config file containing credentials for RDS MySQL instance
 db_username = "admin"
 db_password = "admin1234"
-db_name = "dbmysqla19p2.cuzs16qcrkta.us-east-1.rds.amazonaws.com" 
+db_host = "dbmysqla19p2.cuzs16qcrkta.us-east-1.rds.amazonaws.com"
+db_name = "playgroundA19" 
